9c9935041ef030561218e67c9167659d35513f01
fernandez16 <raulfernan000@gmail.com>
2022-06-15 10:36:17 +0200

    first commit

---
+++ src/main/java/com/shop/onlineShopJava/OnlineShopJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shop.onlineShopJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/onlineShopJava/OnlineShopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.onlineShopJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
